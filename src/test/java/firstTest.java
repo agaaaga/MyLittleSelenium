c0a4b0e03f6b6100d29b159bf8d3357a7676d401
Your Name <you@example.com>
2018-03-19 14:43:21 +0100

    refactoring of test

--- src/test/java/firstTest.java
+++ src/test/java/firstTest.java
@@ -1,26 +1,30 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import static org.assertj.core.api.Assertions.assertThat;
 public class firstTest {
-    public static void main(String[] args){
+    private static WebDriver driver;
+
+    @Before
+    public void setUp(){
         System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
+    }
+    @After
+    public void tearDown(){
+        driver.close();
+    }
+    @Test
+    public void checkIfPracujTitleIsCorrect(){
 
+        String baseURL = "http://www.pracuj.pl";
         String actualTitle = "";
+        String expectedTitle = "Oferty pracy - Pracuj.pl";
 
-        driver.get("http://www.pracuj.pl");
-        actualTitle = driver.getTitle();
-        System.out.println("Tutaj nazwa strony: " + actualTitle);
+        driver.get(baseURL);
 
-        if(actualTitle.contentEquals("Oferty pracy - Pracuj.pl")){
-            System.out.println("Test Passed!");
-        } else {
-            System.out.println("Test Failed!");
-        }
-
-
-
-
-        driver.quit();
+        assertThat(driver.getTitle().contentEquals(expectedTitle));
     }
 }
